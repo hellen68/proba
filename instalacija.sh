b884bbc343c6677213ac56abddbf9b47f5ea9a24
hellen68 <70208213+hellen68@users.noreply.github.com>
2020-09-06 14:52:04 +0200

    Add files via upload

---
+++ instalacija.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "alias program='cd /data/data/com.termux/files/home/proba && bash program'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+source /data/data/com.termux/files/usr/etc/bash.bashrc
