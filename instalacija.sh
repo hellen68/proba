49ad03ec0dd16fb93f578947f609da23bd6830d7
hellen68 <70208213+hellen68@users.noreply.github.com>
2020-09-06 15:11:42 +0200

    Delete instalacija.sh

--- instalacija.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "alias program='cd /data/data/com.termux/files/home/proba && bash program'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-cd /data/data/com.termux/files/usr/etc
-source bash.bashrc
