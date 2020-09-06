7111da2e0b83754c40a86dc2a5cd92b15d0d87a6
hellen68 <70208213+hellen68@users.noreply.github.com>
2020-09-06 15:01:30 +0200

    Update instalacija.sh

--- instalacija.sh
+++ instalacija.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "alias program='cd /data/data/com.termux/files/home/proba && bash program'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-source /data/data/com.termux/files/usr/etc/bash.bashrc
+cd /data/data/com.termux/files/usr/etc
+source bash.bashrc
